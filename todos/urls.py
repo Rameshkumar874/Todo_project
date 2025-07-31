99b862a7e94c099f17eb3ba39882dfce63745346
Rameshkumar874 <rameshkumardap@gmail.com>
2025-07-31 14:40:35 +0530

    todo app final

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
      path('addTask/',views.addTask,name='addTask'),
+     # mark as done
+     path('mark_as_done/<int:pk>/', views.mark_as_done,name='mark_as_done'),
+     # mark as undone
+     path('mark_as_undone/<int:pk>/', views.mark_as_undone,name='mark_as_undone'),
+     # edit features
+     path('edit_task/<int:pk>', views.edit_task,name='edit_task'),
+     #Delete task 
+     path('delete_task/<int:pk>', views.delete_task,name='delete_task'),
+     
 ]
  
--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect,get_object_or_404,render
 from .models import Task
+from django.http import HttpResponse
 
 def addTask(request):
     if request.method == 'POST':
-        task_text = request.POST.get('task')  # 'task' is the name attribute of your input field
+        task_text = request.POST.get('task')  
         if task_text:
             Task.objects.create(task=task_text)
         return redirect('home')  
-    # return redirect('home') 
+    return redirect('home') 
+
+def mark_as_done(request ,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+def edit_task(request, pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    if request.method=="POST":
+        new_task=request.POST['task']
+        get_task.task= new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task
+        }
+        return render(request,'edit_task.html',context)
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home') 
+    
